5f33728fe9c6be79b834ab8f088b91b4adfeb672
Eduarda Gama <eduarda.gama@salg.com.br>
2024-10-18 13:15:48 -0300

    Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.

--- 10-Sequencial-CelsiusParaFahrenheit.py
+++ 10-Sequencial-CelsiusParaFahrenheit.py
@@ -1,4 +1,4 @@
-# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+# 10. Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
 celsius = float(input("qual a temperatura em C?"))
 fahrenheit = (celsius * 9/5) + 32
 print("a temperatura em fahrenheit é ", fahrenheit)
