94e74e821906c3c55626cf9cd416977742938055
Eduarda Gama <eduarda.gama@salg.com.br>
2024-10-18 19:35:11 -0300

    Delete 12-Sequencial-PesoIdeal.py

--- 12-Sequencial-PesoIdeal.py
+++
@@ -1,4 +0,0 @@
-# 12. Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
-altura = float(input("qual sua altura? "))
-pesoIdeal = (72.7* altura) - 58
-print("seu peso ideal é ", "{:.2f}".format(pesoIdeal))
