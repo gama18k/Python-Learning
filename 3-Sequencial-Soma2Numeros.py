8aa10923c82aacda5580623a96c84d271caa129b
Eduarda Gama <eduarda.gama@salg.com.br>
2024-10-18 19:42:52 -0300

    Delete 3-Sequencial-Soma2Numeros.py

--- 3-Sequencial-Soma2Numeros.py
+++
@@ -1,5 +0,0 @@
-# 3. Faça um Programa que peça dois números e imprima a soma.
-numero1 = int(input("Digite um número inteiro: "))
-numero2 = int(input("Digite um número inteiro: "))
-numero3 = numero1 + numero2
-print(f"a soma total é {numero3}")
