c95d269a7de8ef51f98416b8fc70c9af92366221
Eduarda Gama <eduarda.gama@salg.com.br>
2024-10-18 13:11:13 -0300

    Faça um Programa que peça dois números e imprima a soma.

--- 3-Sequencial-Soma2Numeros.py
+++ 3-Sequencial-Soma2Numeros.py
@@ -1,4 +1,4 @@
-# Faça um Programa que peça dois números e imprima a soma.
+# 3. Faça um Programa que peça dois números e imprima a soma.
 numero1 = int(input("Digite um número inteiro: "))
 numero2 = int(input("Digite um número inteiro: "))
 numero3 = numero1 + numero2
