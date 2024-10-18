484264c35464ba3c4c4089df80bcd061f728a697
Eduarda Gama <eduarda.gama@salg.com.br>
2024-10-18 19:43:00 -0300

    Delete 4-Decisao-VogalConsoante.py

--- 4-Decisao-VogalConsoante.py
+++
@@ -1,7 +0,0 @@
-# 4. Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
-vogal = ["a", "e", "i", "o", "u"]
-letra = input(str("digite uma letra: "))
-if letra in vogal:
-     print("letra é vogal")
-else:
-     print("a letra é consoante")
