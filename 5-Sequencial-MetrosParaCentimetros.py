edd33560ee7fdcd587310aa1dedc8f078b4d82e3
Eduarda Gama <eduarda.gama@salg.com.br>
2024-10-18 19:43:54 -0300

    Delete 5-Sequencial-MetrosParaCentimetros.py

--- 5-Sequencial-MetrosParaCentimetros.py
+++
@@ -1,4 +0,0 @@
-# 5. Faça um Programa que converta metros para centímetros.
-metros = float(input("digite os metros:"))
-centimetros = metros * 100
-print(metros, "metros em centímetros é ", centimetros)
