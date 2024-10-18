7fcc6e8756e84014296c2dbbf78430bdb95e7a7c
Eduarda Gama <eduarda.gama@salg.com.br>
2024-10-18 19:34:37 -0300

    Delete 6-Decisao-Maiorde3Numeros.py

--- 6-Decisao-Maiorde3Numeros.py
+++
@@ -1,7 +0,0 @@
-# 6. Faça um Programa que leia três números e mostre o maior deles.
-num1 = int(input("digite o primeiro número: "))
-num2 = int(input("digite o segundo número: "))
-num3 = int(input("digite o terceiro número: "))
-
-maiorNum = max(num1, num2, num3)
-print(f"O maior número é: ", maiorNum)
