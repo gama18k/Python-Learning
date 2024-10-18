8205ed3ab1478edfd8fb0f6c3ff2f60f64e90c46
Eduarda Gama <eduarda.gama@salg.com.br>
2024-10-18 13:14:00 -0300

    Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.

--- 7-Sequencial-DobroAreaQuadrado.py
+++ 7-Sequencial-DobroAreaQuadrado.py
@@ -1,4 +1,4 @@
-# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+# 7. Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
 lado = float(input("qual tamanho do lado do quadrado?"))
 areaQ = lado ** 2
 dobro = areaQ * 2
