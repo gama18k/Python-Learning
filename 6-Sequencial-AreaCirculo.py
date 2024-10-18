93627c5a1e61ce4fda0ff3b6788da50f6d9b7281
Eduarda Gama <eduarda.gama@salg.com.br>
2024-10-18 13:13:01 -0300

    Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.

--- 6-Sequencial-AreaCirculo.py
+++ 6-Sequencial-AreaCirculo.py
@@ -1,4 +1,4 @@
-# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+# 6. Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
 raio = float(input("qual raio do circulo?"))
 area = 3.141 * (raio ** 2)
 print("area igual a ", area)
