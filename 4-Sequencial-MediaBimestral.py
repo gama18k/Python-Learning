543f156b8ef944d5f4a94c31c1500c25e8989b3e
Eduarda Gama <eduarda.gama@salg.com.br>
2024-10-18 19:43:10 -0300

    Delete 4-Sequencial-MediaBimestral.py

--- 4-Sequencial-MediaBimestral.py
+++
@@ -1,7 +0,0 @@
-# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
-nota1 = float(input("digite sua primeira nota:"))
-nota2 = float(input("digite sua primeira nota:"))
-nota3 = float(input("digite sua primeira nota:"))
-nota4 = float(input("digite sua primeira nota:"))
-media = (nota1 + nota2 + nota3 + nota4)/4
-print(f"Sua média é: {media:.2f}")
