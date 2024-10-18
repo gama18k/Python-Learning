0c18bf00201efc8125bd610541f477ea2f7b0f52
Eduarda Gama <eduarda.gama@salg.com.br>
2024-10-18 19:41:12 -0300

    Delete 2-Decisao-PositivoNegativo.py

--- 2-Decisao-PositivoNegativo.py
+++
@@ -1,8 +0,0 @@
-# 2. Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
-
-print("Solução do exercício 2:")
- num = int(input("digite um número:"))
- if (num>0):
-     print("valor positivo")
- else:
-     print("valor negativo")
