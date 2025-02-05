e734bf33458824a8a2c8f4aa5e81ce152fe527b3
Eduarda Gama <eduarda.gama@salg.com.br>
2025-02-05 20:41:11 -0300

    Create int_float.py

---
+++ Estrutura Sequencial/int_float.py
@@ -0,0 +1,3 @@
+# Desenvolva um programa que leia um número inteiro qualquer e que apresente o número informado com duas casas decimais
+numero = int(input('Digite um número inteiro: '))
+print(f"o número com duas casas decimais fica {numero:.2f}")
