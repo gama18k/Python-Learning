0f3ac012513cc32efc8fb1bb2f493a16151a3439
Eduarda Gama <eduarda.gama@salg.com.br>
2025-02-05 20:47:02 -0300

    Create antecessor_sucessor.py

---
+++ Estrutura Sequencial/antecessor_sucessor.py
@@ -0,0 +1,3 @@
+# Desenvolva um programa que leia um número inteiro qualquer e que apresete o número informado, seguido do seu antecessor e do seu sucessor
+numero = int(input('Digite um número inteiro: '))
+print(f"o seu número é: {numero}, o antecessor é: {numero-1} e o sucessor é: {numero+1}")
